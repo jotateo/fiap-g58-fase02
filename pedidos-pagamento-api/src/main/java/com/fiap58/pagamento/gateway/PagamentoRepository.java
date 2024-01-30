b92cf9bd8ccc6189b27a5373326e448663e09182
Lessalc <lucianoclessa@gmail.com>
2024-01-30 18:05:21 -0300

    Adicionando endpoint para buscar pagamentos baseado no id do pedido

--- pedidos-pagamento-api/src/main/java/com/fiap58/pagamento/gateway/PagamentoRepository.java
+++ pedidos-pagamento-api/src/main/java/com/fiap58/pagamento/gateway/PagamentoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
     Pagamento findByQrCode(String qrCode);
+    Pagamento findByIdPedido(Long idPedido);
 }
